10bf95a477756a4348962dadd18f92b8867f6f97
Alex Fivecoats <ultramegacoats@gmail.com>
2018-12-24 14:55:07 -0500

    learning_node tutorial

--- server.js
+++ server.js
@@ -3,28 +3,44 @@ var bodyparser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({extended: false}))
 
-var messages = [
-    {name: 'Tim', message: 'Hi'},
-    {name: 'Jane', message: 'Hello'}
-]
+var dburl = 'mongodb://user:node_2018@ds131313.mlab.com:31313/learning_node'
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
 
 app.get('/messages', (req,res) =>{
-    res.send(messages)
+    Message.find({}, (err, messages) =>{
+        res.send(messages)
+    })
 })
 
 app.post('/messages', (req,res) =>{
-    messages.push(req.body)
-    io.emit('message', req.body)
-    res.sendStatus(200)
+    var message = new Message(req.body)
+
+    message.save((err) => {
+        if(err)
+            sendStatus(500)
+
+        io.emit('message', req.body)
+        res.sendStatus(200)
+    })
+
 })
 
 io.on('connection', (socket) => {
     console.log('a user has connected')
+})
+
+mongoose.connect(dburl, { useNewUrlParser: true }, (err) => {
+    console.log('mongo db connection', err)
 })
 
 var server = http.listen(3000, () => {
